f9295455b2c428cc646a7fa7d3c492dc97627eac
Pranjal Vachaspati <pr@nj.al>
2017-10-05 10:26:23 +0200

    add newick tree processing methods (for postorder traversal and taxon collectino)

--- src/newick.cpp
+++ src/newick.cpp
@@ -30,6 +30,42 @@ int newick_to_ts(const string& s, unordered_set<string>& taxa) {
   return taxon_count;
 }
 
+Clade newick_to_taxa(const string& s, TaxonSet& ts) {
+    typedef boost::tokenizer<boost::char_separator<char> > 
+    tokenizer;
+  boost::char_separator<char> sep(";\n", "():,");
+
+  Clade clade(ts);
+  
+  tokenizer tokens(s, sep);
+  
+  string prevtok = "";
+  
+  for (auto tok : tokens) {
+    
+    if (tok == "(") {
+    }
+    else if (tok == ")") {
+    }
+    else if (tok == ":") {
+    } else if (tok == ",") {
+    } else {
+      if (prevtok == ")" or prevtok == ":" or (tok == " " and prevtok == ",")) {
+	continue;
+      }
+      boost::algorithm::trim(tok);
+      Taxon id = ts[tok];
+
+      clade.add(id);
+      
+
+    }
+    prevtok = tok;
+    
+  }
+  return clade;
+}
+
 void newick_to_dm(const string& s, TaxonSet& ts, dm_type& dist_mat, dm_type& mask_mat ) {
   typedef boost::tokenizer<boost::char_separator<char> > 
     tokenizer;
@@ -122,6 +158,43 @@ void newick_to_clades(const string& s, TaxonSet& ts, unordered_set<Clade>& clade
     clade_set.insert(c);
   }
 }
+
+void newick_to_postorder(const string& s, TaxonSet& ts, vector<Taxon>& order) {
+  typedef boost::tokenizer<boost::char_separator<char> > 
+    tokenizer;
+  boost::char_separator<char> sep(";\n", "():,");
+  string prevtok = "";
+  
+  tokenizer tokens(s, sep);
+
+  vector<int> sizes;
+  sizes.push_back(0);
+  
+  for (auto tok : tokens) {
+    if (tok == "(") {
+      sizes.back()++;
+      sizes.push_back(0);
+    }
+    else if (tok == ")") {
+      order.push_back(-1 * sizes.back());
+      sizes.pop_back();      
+    }
+    else if (tok == ":") {      
+    } else if (tok == ",") {
+    } else {
+      if (prevtok == ")" or prevtok == ":" or (tok == " " and prevtok == ",")) {
+	continue;
+      }
+      sizes.back() ++;
+      boost::algorithm::trim(tok);
+      Taxon id = ts[tok];
+      order.push_back(id);
+    }
+    prevtok = tok;
+  }
+
+}
+
 
 string map_newick_names(const string& s, TaxonSet& ts) {
     typedef boost::tokenizer<boost::char_separator<char> > 
--- src/newick.hpp
+++ src/newick.hpp
@@ -18,8 +18,13 @@ typedef boost::multi_array<double, 2> dm_type;
 
 int newick_to_ts(const string& s, unordered_set<string>& taxa);
 
+
+Clade newick_to_taxa(const string& s, TaxonSet& ts);
 void newick_to_dm(const string& s, TaxonSet& ts, dm_type& dist_mat, dm_type& mask_mat );
 void newick_to_clades(const string& s, TaxonSet& ts, unordered_set<Clade>& clade_set);
+void newick_to_postorder(const string& s, TaxonSet& ts, vector<Taxon>& order);
+
+
 string map_newick_names(const string& s, TaxonSet& ts);
 string unmap_newick_names(const string& s, TaxonSet& ts);
 string unmap_clade_names(const string& s, TaxonSet& ts);
